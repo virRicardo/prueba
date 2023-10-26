98c6abac5f153ebf5bc76e2f2ac1a71223ffbe08
dainnin <36818514+dainnin@users.noreply.github.com>
2023-10-26 07:21:26 -0300

    Create appV2.js

---
+++ logicaVistas/appV2.js
@@ -0,0 +1,9 @@
+
+var botonesEliminarItem = document.getElementsByClassName('btn_eliminar');
+
+this.addEventListener('click',function(e){
+  
+    if(e.target.classList.contains('btn_eliminar')){
+    console.log()
+            alert('')}
+     } );
