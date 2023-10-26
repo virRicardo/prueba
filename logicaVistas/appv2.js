75375b92b05a4f63d98a25d714d75569d4a8b8b9
dainnin <36818514+dainnin@users.noreply.github.com>
2023-10-26 07:29:00 -0300

    Update appv2.js

--- logicaVistas/appv2.js
+++ logicaVistas/appv2.js
@@ -4,6 +4,9 @@ var botonesEliminarItem = document.getElementsByClassName('btn_eliminar');
 this.addEventListener('click',function(e){
   
     if(e.target.classList.contains('btn_eliminar')){
-    console.log()
-            alert('')}
+    //actualizo el total del carrito
+    actualizarTotalCarrito();
+
+    //controlo si hay elementos en el carrito una vez q se elimino y si no hay oculto el carrito
+    ocultarCarrito();}
      } );
